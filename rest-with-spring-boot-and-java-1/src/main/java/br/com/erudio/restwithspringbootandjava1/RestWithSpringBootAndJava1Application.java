34a33d0f72877bfa5d8c76fed37b0d82b69f0d45
Hique Colombo <hique1999@hotmail.com>
2024-04-22 19:00:46 -0300

    First steps

---
+++ rest-with-spring-boot-and-java-1/src/main/java/br/com/erudio/restwithspringbootandjava1/RestWithSpringBootAndJava1Application.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspringbootandjava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJava1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootAndJava1Application.class, args);
+    }
+
+}
